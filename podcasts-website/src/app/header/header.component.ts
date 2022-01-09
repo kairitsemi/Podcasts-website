797c422e5e19cf472f2a0419aebed06fd43f7985
DESKTOP-NLNSFMK\Kairit <kairit.semiskar@hotmail.com>
2022-01-09 14:28:21 +0200

    new podcast page and navigation buttons in header

--- podcasts-website/src/app/header/header.component.ts
+++ podcasts-website/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common'
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private scroller: ViewportScroller) { }
 
   ngOnInit(): void {
   }
 
+  goToLatest() {
+    this.scroller.scrollToAnchor("latestEpisodes");
+  }
+
+  goToPopular() {
+    this.scroller.scrollToAnchor("popularEpisodes");
+  }
+
+  goToAll() {
+    this.scroller.scrollToAnchor("allEpisodes");
+  }
 }
--- podcasts-website/src/app/podcasts/podcasts.component.ts
+++ podcasts-website/src/app/podcasts/podcasts.component.ts
@@ -1,8 +1,5 @@
-import { ActivatedRoute } from '@angular/router';
-import { PodcastsRss } from './podcasts-rss';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import * as xml2js from 'xml2js';
+
 
 
 @Component({
@@ -11,45 +8,11 @@ import * as xml2js from 'xml2js';
   styleUrls: ['./podcasts.component.scss']
 })
 export class PodcastsComponent implements OnInit {
-  RssData: any;
 
-  constructor(private http: HttpClient) {
 
-  }
+  constructor() { }
 
   ngOnInit(): void {
-    this.GetRssFeedData();
   }
-  GetRssFeedData() {
-    const requestOptions: Object = {
-      observe: 'body',
-      responseType: 'text',
-    };
-    const _url = "https://feeds.soundcloud.com/users/soundcloud:users:881573947/sounds.rss";
 
-    this.http
-      .get<any>(
-        _url,
-        requestOptions
-      )
-      .subscribe((data) => {
-        console.log(data);
-
-        let parseString = xml2js.parseString;
-        parseString(data, (err, result: PodcastsRss) => {
-          this.RssData = result;
-        });
-      });
-  }
-   getDataDiff(endDate: any) {
-    let setDate= new Date(endDate).toISOString();
-    var diff = (new Date()).getTime() - new Date(setDate).getTime();
-    var days = Math.floor(diff / (60 * 60 * 24 * 1000));
-    var hours = Math.floor(diff / (60 * 60 * 1000)) - (days * 24);
-    var minutes = Math.floor(diff / (60 * 1000)) - ((days * 24 * 60) + (hours * 60));
-    let dayString =  days == 0 ?  "" : days + "days ";
-    let hoursString =  hours == 0 ?  "" : hours + "hr ";
-    let minutesString =  minutes == 0 ?  "" : minutes + "m ";
-    return dayString + hoursString + minutesString;
-  }
 }
