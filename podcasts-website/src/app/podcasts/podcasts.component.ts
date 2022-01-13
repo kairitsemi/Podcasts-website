6329683aeb69fabba54987818a6913f766d69865
DESKTOP-NLNSFMK\Kairit <kairit.semiskar@hotmail.com>
2022-01-13 13:16:03 +0200

    some changes in podcast component

--- podcasts-website/src/app/podcasts/podcasts.component.ts
+++ podcasts-website/src/app/podcasts/podcasts.component.ts
@@ -2,18 +2,18 @@ import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 
-
 @Component({
   selector: 'app-podcasts',
   templateUrl: './podcasts.component.html',
   styleUrls: ['./podcasts.component.scss']
 })
 export class PodcastsComponent implements OnInit {
-
+episodes: any[] = [];
 
   constructor(private viewportScroller: ViewportScroller) { }
 
   ngOnInit(): void {
+
   }
 
 
