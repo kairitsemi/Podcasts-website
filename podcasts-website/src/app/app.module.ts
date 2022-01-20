cbe154eecc85a24df6983e129ad918b0c2d36fb5
stina <stinalaura.parg@gmail.com>
2022-01-20 17:16:48 +0200

    Browser gave me an error and then  I just removed and re-added ShoppingCartComponent from app.module

--- podcasts-website/src/app/app.module.ts
+++ podcasts-website/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShopComponent } from './shop/shop.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,7 +25,7 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
     HomeComponent,
     PodcastsComponent,
     ShopComponent,
-    ShoppingCartComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
