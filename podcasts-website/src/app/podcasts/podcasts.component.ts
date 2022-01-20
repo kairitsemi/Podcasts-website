d0edef82855b3f59632d43eb0c5dc9e282aac6e1
stina <stinalaura.parg@gmail.com>
2022-01-20 16:56:51 +0200

    Improved shop component

--- podcasts-website/src/app/podcasts/podcasts.component.ts
+++ podcasts-website/src/app/podcasts/podcasts.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './podcasts.component.html',
   styleUrls: ['./podcasts.component.scss']
 })
+
 export class PodcastsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+
   }
 
 }
