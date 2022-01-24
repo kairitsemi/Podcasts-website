0bc247d6739c45ccfcd4da76c16f3716307f6c07
DESKTOP-NLNSFMK\Kairit <kairit.semiskar@hotmail.com>
2022-01-24 13:38:07 +0200

    modified episode component and made contact page form a reactive form

--- podcasts-website/src/app/contact/contact.component.ts
+++ podcasts-website/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  contactForm: FormGroup = { } as FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.contactForm = new FormGroup({
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
+      'subject' : new FormControl(null, Validators.required),
+      'message' : new FormControl(null, [Validators.required, Validators.minLength(10)])
+    })
   }
 
+  submit() {
+    console.log(this.contactForm.value);
+    this.contactForm.reset();
+
+  }
+
+  get email() {
+    return this.contactForm.get('email')
+  }
+
+  get subject() {
+    return this.contactForm.get('subject')
+  }
+
+  get message() {
+    return this.contactForm.get('message')
+  }
 }
